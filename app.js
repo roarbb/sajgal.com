908eed688bff28398dff463567c1a4bd19af2365
Matej Sajgal <roarbb@Matejs-MacBook-Pro.local>
2017-12-20 00:18:49 +1100

    express app

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var port = process.env.PORT || 3000;
 
 app.use('/assets', express.static(__dirname + '/assets'));
 app.use('/css', express.static(__dirname + '/css'));
