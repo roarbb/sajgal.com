8fc4aa0ac7654f995f723a23d297717e0d479639
Matej Sajgal <roarbb@Matejs-MacBook-Pro.local>
2017-12-20 00:22:11 +1100

    express app

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
 
-app.use('/assets', express.static(__dirname + '/assets'));
-app.use('/css', express.static(__dirname + '/css'));
+app.get('/', function(request, response) {
+    response.sendFile(__dirname + '/index.html')
+})
 
-app.listen(port, function() { console.log('listening')});
+app.use('/assets', express.static(__dirname + '/assets'))
+app.use('/css', express.static(__dirname + '/css'))
+
+app.listen(port)
