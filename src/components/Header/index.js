c28bc451cb92bbf242e22773b75d4711e904c970
Matej Sajgal <roarbb@gmail.com>
2018-03-07 22:30:58 +1100

    basic styles

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,33 +1,17 @@
-import React from 'react'
-import Link from 'gatsby-link'
+import React from "react";
+import Link from "gatsby-link";
 
 const Header = () => (
-  <div
-    style={{
-      background: '#bada55',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'hsla(0, 0%, 0%, .8)',
-            textDecoration: 'none',
-          }}
-        >
-          sajgal.com
-        </Link>
-      </h1>
-    </div>
-  </div>
-)
+  <header>
+    <div className="logo">SAJGAL.COM</div>
+    <nav className="main-nav">
+      <ul>
+        <li>About</li>
+        <li>Projects</li>
+        <li>Work</li>
+      </ul>
+    </nav>
+  </header>
+);
 
-export default Header
+export default Header;
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,35 +1,34 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Helmet from 'react-helmet'
+import React from "react";
+import PropTypes from "prop-types";
+import Helmet from "react-helmet";
 
-import Header from '../components/Header'
-import './index.css'
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import video from "../assets/sajgal-bike-bg.mp4";
+import "./index.css";
 
 const TemplateWrapper = ({ children }) => (
-  <div>
+  <div className="grid">
     <Helmet
       title="Gatsby Default Starter"
       meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
+        { name: "description", content: "Sample" },
+        { name: "keywords", content: "sample, something" }
       ]}
     />
+    
     <Header />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
-      {children()}
-    </div>
+
+    <video autoPlay loop muted src={video} />
+
+    <main>{children()}</main>
+
+    <Footer />
   </div>
-)
+);
 
 TemplateWrapper.propTypes = {
-  children: PropTypes.func,
-}
+  children: PropTypes.func
+};
 
-export default TemplateWrapper
+export default TemplateWrapper;
